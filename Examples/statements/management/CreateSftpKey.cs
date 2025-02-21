06a3423fb7c03ac077a4dbe50553f2afcd84b9c2
Palloma Brito <palloma.brito@sejaefi.com.br>
2025-02-21 14:54:55 -0300

    ✨ Exemplo para criar chave sftp

---
+++ Examples/statements/management/CreateSftpKey.cs
@@ -0,0 +1,26 @@
+
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    internal class CreateSftpKey
+    {
+        public static void Execute()
+        {
+
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            try
+            {
+                var response = efi.CreateSftpKey();
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
