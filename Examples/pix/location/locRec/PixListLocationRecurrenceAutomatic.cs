9274615e4aaffd61e3ae2b16307853712b39fdb7
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:51:25 -0300

    ✨ Consultar locations de recorrência de Pix Automático cadastradas

---
+++ Examples/pix/location/locRec/PixListLocationRecurrenceAutomatic.cs
@@ -0,0 +1,31 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixListLocationRecurrenceAutomatic
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                inicio = "2023-12-01T00:00:00Z",
+                fim = "2024-04-01T23:59:59Z"
+            };
+
+            try
+            {
+                var response = efi.PixListLocationRecurrenceAutomatic(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
