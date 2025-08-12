3a5ae2c4171a735cf57222cea30fcf25f38ae3bf
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:31:27 -0300

    ✨ Consultar pagamentos via Jornada sem Redirecionamento

---
+++ Examples/open-finance/payments/biometric/OfListBiometricPixPayment.cs
@@ -0,0 +1,32 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    internal class OfListBiometricPixPayment
+    {
+        public static void Execute()
+        {
+
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                inicio = "2022-04-29",
+                fim = "2024-12-29"
+            };
+
+            try
+            {
+                var response = efi.OfListBiometricPixPayment(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
