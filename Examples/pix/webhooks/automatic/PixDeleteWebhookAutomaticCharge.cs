5696618baba85cd071d05592073951256dc46256
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:56:32 -0300

    ✨ Cancelar o webhook de cobrança de Pix Automático

---
+++ Examples/pix/webhooks/automatic/PixDeleteWebhookAutomaticCharge.cs
@@ -0,0 +1,25 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    internal class PixDeleteWebhookAutomaticCharge
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            try
+            {
+                var response = efi.PixDeleteWebhookAutomaticCharge(null);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
