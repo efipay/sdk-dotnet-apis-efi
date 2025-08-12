06371daf9e64e709b82d9931cb4a11c9f35ba54d
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:28:37 -0300

    ✨ Consultar vínculos de um usuário

---
+++ Examples/open-finance/payments/biometric/OfListBiometricEnrollment.cs
@@ -0,0 +1,33 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
+
+namespace Examples
+{
+    internal class OfListBiometricEnrollment
+    {
+        public static void Execute()
+        {
+
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                cpf = "45204392050"
+            };
+
+            try
+            {
+                var response = efi.OfListBiometricEnrollment(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
