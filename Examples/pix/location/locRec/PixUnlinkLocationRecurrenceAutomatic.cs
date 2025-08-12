2c8d20f9aa8f745b5d616f74e25ff9e4e1d3c2f9
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:52:29 -0300

    ✨ Desvincular uma recorrência de Pix Automático de um location

---
+++ Examples/pix/location/locRec/PixUnlinkLocationRecurrenceAutomatic.cs
@@ -0,0 +1,30 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixUnlinkLocationRecurrenceAutomatic
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                id = 12069
+            };
+
+            try
+            {
+                var response = efi.PixUnlinkLocationRecurrenceAutomatic(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
