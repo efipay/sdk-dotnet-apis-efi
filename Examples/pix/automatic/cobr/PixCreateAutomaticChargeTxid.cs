703689fe779c46890fd8dbafdc0c24179e4b52a0
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:40:21 -0300

    ✨ Criar cobrança de Pix Automático (com txid)

---
+++ Examples/pix/automatic/cobr/PixCreateAutomaticChargeTxid.cs
@@ -0,0 +1,60 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+using System.Runtime.ConstrainedExecution;
+
+namespace Examples
+{
+    class PixCreateAutomaticChargeTxid
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                txid = "517bd858b59d458a841280b0f0a60bfa"
+            };
+
+            var body = new
+            {
+                idRec = "RR1234567820240115abcdefghijk",
+                infoAdicional = "Serviços de Streamming de Música e Filmes.",
+                calendario = new
+                {
+                    dataDeVencimento = "2024-04-15"
+                },
+                valor = new
+                {
+                    original = "106.07"
+                },
+                ajusteDiaUtil = true,
+                devedor = new
+                {
+                    cep = "89256140",
+                    cidade = "Uberlândia",
+                    email = "sebastiao.tavares@mail.com",
+                    logradouro = "Alameda Franco 1056",
+                    uf = "MG"
+                },
+                recebedor = new
+                {
+                    agencia = "9708",
+                    conta = "12682",
+                    tipoConta = "CORRENTE"
+                }
+            };
+
+            try
+            {
+                var response = efi.PixCreateAutomaticChargeTxid(param, body);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
