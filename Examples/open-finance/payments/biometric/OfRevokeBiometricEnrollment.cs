569d01ed13e723e41120951ae47f93fea6b030c2
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:29:18 -0300

    ✨ Revogar vinculo

---
+++ Examples/open-finance/payments/biometric/OfRevokeBiometricEnrollment.cs
@@ -0,0 +1,34 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
+
+namespace Examples
+{
+    internal class OfRevokeBiometricEnrollment
+    {
+        public static void Execute()
+        {
+
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var body = new
+            {
+                identificadorVinculo = "urn:efi:ae71713f-875b-4af3-9d85-0bcb43288847",
+                motivo = "Encerramento de contrato de vinculo"
+            };
+
+            try
+            {
+                var response = efi.OfRevokeBiometricEnrollment(body);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
