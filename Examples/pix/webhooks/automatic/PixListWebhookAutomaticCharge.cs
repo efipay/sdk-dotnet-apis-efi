c818e476ec441decbd0ddec9a406c5a961c6f887
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:56:06 -0300

    ✨ Exibir informações do webhook de cobrança de Pix Automático

---
+++ Examples/pix/webhooks/automatic/PixListWebhookAutomaticCharge.cs
@@ -0,0 +1,25 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    internal class PixListWebhookAutomaticCharge
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            try
+            {
+                var response = efi.PixListWebhookAutomaticCharge(null);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
