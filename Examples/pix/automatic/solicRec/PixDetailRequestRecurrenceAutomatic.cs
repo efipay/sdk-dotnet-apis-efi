79b6fe0a3c2dc68ad222f8010f73f02deeed87cc
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:39:03 -0300

    ✨ Consultar solicitação de confirmação de recorrência de Pix Automático

---
+++ Examples/pix/automatic/solicRec/PixDetailRequestRecurrenceAutomatic.cs
@@ -0,0 +1,30 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixDetailRequestRecurrenceAutomatic
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                idSolicRec = "SC876456782024021577825445312"
+            };
+
+            try
+            {
+                var response = efi.PixDetailRequestRecurrenceAutomatic(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
