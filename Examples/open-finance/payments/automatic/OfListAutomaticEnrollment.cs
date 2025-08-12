debc29c1afd7cebbb3f6cd31db5284c8ae8fefac
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:32:40 -0300

    ✨ Listar adesões

---
+++ Examples/open-finance/payments/automatic/OfListAutomaticEnrollment.cs
@@ -0,0 +1,32 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    internal class OfListAutomaticEnrollment
+    {
+        public static void Execute()
+        {
+
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                inicio = "2022-04-29",
+                fim = "20242-12-29"
+            };
+
+            try
+            {
+                var response = efi.OfListAutomaticEnrollment(param, null);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
