97beb11b94176626b79bae43821a4dc5ab2bccef
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:36:00 -0300

    ✨ Consultar recorrência de Pix Automático

---
+++ Examples/pix/automatic/jornada2/PixDetailRecurrenceAutomatic.cs
@@ -0,0 +1,30 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixDetailRecurrenceAutomatic
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                idRec = "RN1234567820240115abcdefghijk"
+            };
+
+            try
+            {
+                var response = efi.PixDetailRecurrenceAutomatic(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
