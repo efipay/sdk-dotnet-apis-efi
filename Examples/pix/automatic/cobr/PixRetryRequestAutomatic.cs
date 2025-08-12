029855d12bfdcfa3d6030bf098a0e1baed0d6c52
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:42:59 -0300

    ✨ Solicitar retentativa de Pix Automático

---
+++ Examples/pix/automatic/cobr/PixRetryRequestAutomatic.cs
@@ -0,0 +1,31 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixRetryRequestAutomatic
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                txid = "7f733863543b4a16b516d839bd4bc34e",
+                data = "2024-05-20"
+            };
+
+            try
+            {
+                var response = efi.PixRetryRequestAutomatic(param);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
