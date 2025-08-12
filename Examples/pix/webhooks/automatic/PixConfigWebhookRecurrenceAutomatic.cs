874e8ff375cd181fb5d37c0bf62c46054aaeb9d0
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:54:29 -0300

    ✨ Configurar o webhook de recorrência de Pix Automático

---
+++ Examples/pix/webhooks/automatic/PixConfigWebhookRecurrenceAutomatic.cs
@@ -0,0 +1,32 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    internal class PixConfigWebhookRecurrenceAutomatic
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var headers = "{\"x-skip-mtls-checking\": \"true\"}";
+
+            var body = new
+            {
+                webhookUrl = "https://usuario.recebedor.com/api/webhookrec/"
+            };
+
+            try
+            {
+                var response = efi.PixConfigWebhookRecurrenceAutomatic(body, headers);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
