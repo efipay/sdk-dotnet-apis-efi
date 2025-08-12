9b50c715a2b13067a9b52d7e36b4f2500426b9d5
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-12 15:22:24 -0300

    ✨ Obter comprovantes de transações Pix realizadas via API

---
+++ Examples/exclusives/pix/PixGetReceipt.cs
@@ -0,0 +1,30 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixGetReceipt
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                txid = "7978c0c97ea847e78e8849634473c1f1"
+            };
+
+            try
+            {
+                var response = efi.PixGetReceipt(param, null);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
