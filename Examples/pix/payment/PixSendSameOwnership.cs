2a376620a1370a832c91f97e88ea106f11481ad0
DeboraAmaralEfiBank <debora.amaral@sejaefi.com.br>
2025-08-20 11:20:02 -0300

    ✨ Requisitar envio de Pix para mesma titularidade

---
+++ Examples/pix/payment/PixSendSameOwnership.cs
@@ -0,0 +1,45 @@
+﻿
+using System; using System.IO; using Newtonsoft.Json.Linq;
+using Efipay;
+
+namespace Examples
+{
+    class PixSendSameOwnership
+    {
+        public static void Execute()
+        {
+            dynamic efi = new EfiPay(JObject.Parse(File.ReadAllText("credentials.json")));
+
+            var param = new
+            {
+                idEnvio = 12345
+            };
+
+            var body = new
+            {
+                valor = "12.34",
+                pagador = new
+                {
+                    chave = "19974764017",
+                    infoPagador = "Segue o pagamento da conta"
+                },
+                favorecido = new
+                {
+                    chave = "joão@meuemail.com"
+                }
+            };
+
+            try
+            {
+                var response = efi.PixSendSameOwnership(param, body);
+                Console.WriteLine(response);
+            }
+            catch (EfiException e)
+            {
+                Console.WriteLine(e.ErrorType);
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
